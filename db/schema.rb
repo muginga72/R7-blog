db2934c29a4e853fbb3eeb9660d4acabd7302cb0
muginga72 Public <lmj.muginga@gmail.com Public>
2023-11-10 15:51:05 -0500

    Week 6 - first association commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_06_021844) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_09_233218) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "pages", force: :cascade do |t|
     t.string "title"
     t.text "body"
